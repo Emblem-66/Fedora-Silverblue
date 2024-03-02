c2c4b60dac23e9d6202ddf91435aa48525e6fd10
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-03-02 01:00:08 +0100

    Delete rootfs/usr/loca/bin directory

--- rootfs/usr/loca/bin/first_boot_check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-setup_file="~/.config/first_boot_true"
-
-if [ ! -f "$setup_file" ]; then
-    /usr/local/bin/first_boot_script.sh
-    touch "$setup_file"
-fi
--- rootfs/usr/loca/bin/first_boot_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Running first boot script"
-# Your commands or tasks go here
